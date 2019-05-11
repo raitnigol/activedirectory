0f73aa7ededc029048096395421afa60275875f9
Nigol <49141808+raitnigol@users.noreply.github.com>
2019-05-11 16:29:31 -0700

    Add files via upload
    Korrigeeritud faili.

--- activedirectory_script.ps1
+++ activedirectory_script.ps1
@@ -18,21 +18,27 @@
     CSV tabel peab olema järgneval kujul:
     EESNIMI PEREKONNANIMI ROLL
     Rait    Nigol         IT
+
     Seda sellepärast, et skript töötab antud hetkel ainult nende pealkirjade järgi.
-    Neid on alati võimalik muuta vastavalt oma soovile ning skripti laiet haarendada.
-    Kasutades skripti väljastatakse tulemus $eesnimi.$perekonnanimi $roll
-    ehk Rait.Nigol IT.
+    Neid on alati võimalik muuta vastavalt oma soovile ning niimoodi saab
+    skripti laiet haarendada.
+    Kasutades skripti saame teha suurtes kogustes kasutajaid.
+    CSV faili abil saame muutujad $roll, $eesnimi ja $perenimi, mis on AD
+    kontode loomiseks vajalikud.
 #>
 
-# kasutaja peab sisestama CSV faili asukoh
-# Importime kõigepealt activedirectory mooduli, et skript üldse töötaks.
-# kui skripti ei jooksutata Windows Serveri peal, millel oleks Active Directory olemas,
-# võib see koodijupp errorit visata.
-# Seega peame alguses proovima, kas moodulit on võimalik importida ning kui ei ole,
-# kuvatakse kasutajale, et midagi on valesti ning fail talletatakse samma kohta, kus asub
-# skript.
+<#  
+    Importime kõigepealt activedirectory mooduli, et skript üldse töötaks.
+    Kui skripti ei jooksutata Windows Serveri peal, millel oleks
+    Active Directory olemas, võib see koodijupp errorit visata.
+    Seega peame alguses proovima, kas moodulit on võimalik importida ning kui ei ole,
+    kuvatakse kasutajale, et midagi on valesti ning
+    skript suletakse neli sekundit hiljem.
+    Samuti salvestatakse tekstifail asukohta C:\CSV\errors.txt
+#>
 
 Try {
+    # proovime importida activedirectory
     Start-Sleep -s 1
     Import-Module activedirectory
 }
@@ -45,52 +51,87 @@ Catch {
     Exit
 }
 
-# Võtame CSV faili ning talletame selle info muutujasse $kasutajad.
-# $kasutajad = Import-Csv -Path KETAS:\Asukoht\Sinu\Failinimi.csv
-# PALUN ASETADA CSV FAIL ASUKOHTA C:\CSV\DATA.csv !!!!!
+<#  Võtame CSV faili ning talletame selle info muutujasse $kasutajad.
+    $kasutajad = Import-Csv -Path KETAS:\Asukoht\Sinu\Failinimi.csv
+    PALUN ASETADA CSV FAIL ASUKOHTA C:\CSV\DATA.csv !!!!!
+    Iga kasutaja kohta meie failis, väljastame kahte sorti muutujad -
+    staatilised kui ka muutuvad.
+#>
+
 $kasutajad = Import-Csv C:\CSV\DATA.csv
-# Iga kasutaja kohta, kes asub meie failis, väljastame me muutujad erinevad muutujad.
 
-# nüüd loome muutujad, mis on pidevas muutuses ning ei jää kõikidele kasutajatele samaks.
-
+    # iga kasutaja kohta CSV failis me loome neile staatilised muutujad
+    # kui ka muutuvad muutujad.
+    # Alustame muutuvate muutujatega.
 ForEach ($user in $kasutajad)
 {
+    # loome kasutajale eesnime (Rait)
     $Firstname = $($user.eesnimi)
+    # loome kasutajale perekonnanime (Nigol)
     $Lastname = $($user.perenimi)
-    $Username = $("$Firstname.$Lastname")
-    $Password = $("Passw0rd")
+    # loome kasutajale kasutajanime kujul eesnimi.perenimi (rait.nigol)
+    $Username = $("$Firstname.$Lastname").ToLower()
+    # loome kasutajale rolli (IT)
     $jobtitle = $($user.roll)
-    $principalname = $("$Username@$domain")
-    $email = $("$Username@$gmail").ToLower()
-    # samuti lisame muutujad, mis kõikide kasutajate vahel samaks
-    $domain = $("mukri.sise")
-    $gmail = $("gmail.com")
-
-    # enne kui loome AD teekonna (Pathi) on meil valida kahe kausta vahelt.
-    # Mõned töötajad lähevad alamkausta Vaki, samas kui teised lähevad Massusse.
-    # peame looma if statementi, mis tuvastaks ära, kuhu antud inimese asetama peab.
     
-    # inimesed, kes töötavad Massus - 
+    # enne kui loome j2rgmised muutujad, peame paika panema m6ned staatilised muutujad
+    # loome domeeni mukri.sise
+    $domain = $("mukri.sise")
+    # loome kasutajale gmaili l6pu, mis hiljem liidetakse emailile, et saada email
+    # kujul (rait.nigol@gmail.com)
+    $gmail = $("gmail.com")
+    # loome igale kasutajale sama parooli, sest niikuinii on see tarvis j2rgmisel
+    # sisselogimisel 2ra muuta
+    $Password = $("Passw0rd")
+    # loome kasutajale peamise nime kujul kasutajanimi@domeen (rait.nigol@mukri.sise)
+    $principalname = $("$Username@$domain")
+    # loome kasutajale emaili kasutades muutuja $gmail abi.
+    $email = $("$Username@$gmail").ToLower()
+
+<# 
+    Meie systeemis on kaks alamkausta - Massu ja Vaki.
+    Enne kui loome AD teekonna (pathi), on meil valida kahe kausta vahelt.
+    Mõned töötajad lähevad alamkausta Vaki, samas kui teised lähevad Massusse.
+    Peame looma if statementi, mis tuvastaks ära, kuhu antud inimese asetama peab.
+#>
+
+<#
+    Loome if statementi inimestele, kes töötavad Massus.
+    Erinevate rollide puhul on erinevad pathid, seega peame pidevalt muutma
+    antud teekonda.
+#>
+    # Kui kasutajal on IT roll, on teekond j2rgmine:
+    # mukri.sise/massu/inimesed/it 
     if ($jobtitle -eq "IT") {
         $OU = $("OU=it,OU=inimesed,OU=massu,DC=mukri,DC=sise")
         }
-
+    
+    # Kui kasutajal on Turundusosakonna roll, on teekond j2rgmine:
+    # mukri.sise/massu/inimesed/turundusosakond 
     if ($jobtitle -eq "Turundusosakond") {
         $OU = $("OU=turundusosakond,OU=inimesed,OU=massu,DC=mukri,DC=sise")
         }
 
-    #inimesed, kes töötavad Vakis -
+    # Loome if statementi inimestele, kes töötavad Vakis.
+    # Kui kasutajal on Giidi roll, on teekond j2rgmine:
+    # mukri.sise/vaki/inimesed/giidid
     if ($jobtitle -eq "Giidid") {
         $OU = $("OU=giidid,OU=inimesed,OU=vaki,DC=mukri,DC=sise")
         }
 
+    # Kui kasutajal on Raamatupidaja roll, on teekond j2rgmine:
+    # mukri.sise/vaki/inimesed/raamatupidamine
     if ($jobtitle -eq "Raamatupidamine") {
         $OU = $("OU=raamatupidamine,OU=inimesed,OU=vaki,DC=mukri,DC=sise")
         }
+<#
+    Viimase asjana enne l6petust kontrollime yle, kas antud kasutajad
+    juba eksisteerivad systeemis v6i mitte. Kui kasutajaid ei eksisteeri,
+    hakatakse kasutajaid tegema. Vastasel juhul kuvatakse veateade, et
+    kasutaja juba eksisteerib.
+#>
 
-# $OU = $("CN=$Firstname $Lastname,OU=$jobtitle,OU=Inimesed,$it,DC=mukri,DC=sise")
-
-# Kontrollime üle, kas antud kasutaja juba asub Active Directorys või mitte.
+    # Kontrollime üle, kas antud kasutaja juba asub Active Directorys või mitte.
     if (Get-ADUser -F {SamAccountName -eq $Username})
     {
     # Kui kasutaja eksisteerib, anname sellest kasutajale teada.
