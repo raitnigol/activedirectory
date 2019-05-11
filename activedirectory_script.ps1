3512924b25e4b7b70622bdaf017330aa7e885a17
Nigol <49141808+raitnigol@users.noreply.github.com>
2019-05-11 15:40:57 -0700

    Add files via upload
    Parandatud kujul skript, mis toimib.

--- activedirectory_script.ps1
+++ activedirectory_script.ps1
@@ -31,6 +31,7 @@
 # Seega peame alguses proovima, kas moodulit on võimalik importida ning kui ei ole,
 # kuvatakse kasutajale, et midagi on valesti ning fail talletatakse samma kohta, kus asub
 # skript.
+
 Try {
     Start-Sleep -s 1
     Import-Module activedirectory
@@ -38,23 +39,20 @@ Try {
 Catch {
     # kui kood lööb errorit, väljastatakse veateade.
     Import-Module activedirectory 2>> C:\CSV\errors.txt
-    Write-Warning "Miskit läks valesti. moodulit $module pole võimalik importida"
+    Write-Warning "Miskit läks valesti. moodulit pole võimalik importida"
     Write-Warning "Error salvestatud asukohta C:\CSV\errors.txt"
     Start-Sleep -s 4
     Exit
 }
 
-# väljastame echo-ga rea, mis näitab meile, kust skript üldse algab.
-
-echo ===== KASUTAJAINFO =====
-
 # Võtame CSV faili ning talletame selle info muutujasse $kasutajad.
-# $kasutajad = Import-Csv -Path KETAS:\Asukoht\Sinu\Failini.csv
+# $kasutajad = Import-Csv -Path KETAS:\Asukoht\Sinu\Failinimi.csv
 # PALUN ASETADA CSV FAIL ASUKOHTA C:\CSV\DATA.csv !!!!!
-
+$kasutajad = Import-Csv C:\CSV\DATA.csv
 # Iga kasutaja kohta, kes asub meie failis, väljastame me muutujad erinevad muutujad.
 
 # nüüd loome muutujad, mis on pidevas muutuses ning ei jää kõikidele kasutajatele samaks.
+
 ForEach ($user in $kasutajad)
 {
     $Firstname = $($user.eesnimi)
@@ -65,14 +63,8 @@ ForEach ($user in $kasutajad)
     $principalname = $("$Username@$domain")
     $email = $("$Username@$gmail").ToLower()
     # samuti lisame muutujad, mis kõikide kasutajate vahel samaks
-    $city = $("Tartu")
     $domain = $("mukri.sise")
     $gmail = $("gmail.com")
-    $company = $("Mukri OÜ")
-    $country = $("Eesti")
-    $zipcode = $('666666')
-    $streetaddress = $("Sillaaluse 33")
-    $state = $("Tartumaa")
 
     # enne kui loome AD teekonna (Pathi) on meil valida kahe kausta vahelt.
     # Mõned töötajad lähevad alamkausta Vaki, samas kui teised lähevad Massusse.
@@ -80,24 +72,20 @@ ForEach ($user in $kasutajad)
     
     # inimesed, kes töötavad Massus - 
     if ($jobtitle -eq "IT") {
-        $massu = $("IT")
-        $OU = $("OU=$jobtitle,OU=$massu,DC=mukri,DC=sise")
+        $OU = $("OU=it,OU=inimesed,OU=massu,DC=mukri,DC=sise")
         }
 
     if ($jobtitle -eq "Turundusosakond") {
-        $massu = $("Turundusosakond")
-        $OU = $("OU=$jobtitle,OU=$massu,DC=mukri,DC=sise")
+        $OU = $("OU=turundusosakond,OU=inimesed,OU=massu,DC=mukri,DC=sise")
         }
 
     #inimesed, kes töötavad Vakis -
     if ($jobtitle -eq "Giidid") {
-        $vaki = $("Giidid")
-        $OU = $("OU=$jobtitle,OU=$vaki,DC=mukri,DC=sise")
+        $OU = $("OU=giidid,OU=inimesed,OU=vaki,DC=mukri,DC=sise")
         }
 
     if ($jobtitle -eq "Raamatupidamine") {
-        $vaki = $("Raamatupidamine")
-        $OU = $("OU=$jobtitle,OU=$vaki,DC=mukri,DC=sise")
+        $OU = $("OU=raamatupidamine,OU=inimesed,OU=vaki,DC=mukri,DC=sise")
         }
 
 # $OU = $("CN=$Firstname $Lastname,OU=$jobtitle,OU=Inimesed,$it,DC=mukri,DC=sise")
@@ -110,8 +98,8 @@ ForEach ($user in $kasutajad)
     }
     else
     {
-    # Kui kasutaja ei eksisteeri AD-s, siis loome selle kasutaja.
-        New-ADUser -SamAccountName $Username -UserPrincipalName $principalname -Name "$Firstname $Lastname" -GivenName $Firstname -Surname $Lastname -Enabled $True -DisplayName "Lastname, $Firstname" -Path $OU -City $city -Company $company -State $state -EmailAddress $email -Title $jobtitle -Department $jobtitle -AccountPassword (convertto-securestring $Password -AsPlainText -Force) -ChangePasswordAtLogon $True
+        # Kui kasutaja ei eksisteeri AD-s, siis loome selle kasutaja.
+        New-ADUser -SamAccountName $Username -UserPrincipalName $principalname -Name "$Firstname $Lastname" -GivenName $Firstname -Surname $Lastname -Enabled $True -DisplayName "Lastname, $Firstname" -Path $OU -EmailAddress $email -Title $jobtitle -Department $jobtitle -ChangePasswordAtLogon $True -AccountPassword (convertto-securestring $Password -AsPlainText -Force)
     }
 
 }
